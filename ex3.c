7a1e313f62e429960c80cf5ab6054b11ef20c6d1
Álex Porto <alexportoferreira@hotmail.com>
2019-04-08 09:38:35 -0300

    Add ex3,about hanoi´s tower

---
+++ ex3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int move_disc(int disc,char ori, char dest, char aux){
+    if (disc == 1) {
+        printf("Mover disco %d de %c para %c\n", disc, ori, dest);
+    }
+    else
+    {
+        move_disc(disc-1,ori,aux,dest);
+        printf("Mover disco %d de %c para %c\n", disc, ori, dest);
+        move_disc(disc-1,aux,dest,ori);
+    }
+}
+
+int main(){
+    int discos;
+    printf("Por favor,insira o numero de discos\n");
+    scanf("%d",&discos);
+    move_disc(discos,'A','C', 'B');
+    return 0;
+}
